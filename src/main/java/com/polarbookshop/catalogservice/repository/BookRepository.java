cc980b0a4c2b7376eb2a54377b6d99ed7cfdf713
chasethesky79 <seshadri.bharath@yahoo.com>
2024-05-26 14:52:55 -0500

    Implemented the repository layer

---
+++ src/main/java/com/polarbookshop/catalogservice/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package com.polarbookshop.catalogservice.repository;
+
+import com.polarbookshop.catalogservice.domain.Book;
+
+import java.util.Optional;
+
+public interface BookRepository {
+    Iterable<Book> findAll();
+    Optional<Book> findByIsbn(final String isbn);
+    boolean existsByIsbn(final String isbn);
+    Book save(final Book book);
+}
