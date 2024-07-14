136c4b48854b6fd3b05de62b83c7c9e66f088499
chasethesky79 <seshadri.bharath@yahoo.com>
2024-07-14 16:40:55 -0500

    Implemented a spring boot integration test

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,13 +1,40 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest (
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 class CatalogServiceApplicationTests {
 
+	private final WebTestClient webTestClient;
+
+	@Autowired
+	public CatalogServiceApplicationTests(final WebTestClient webTestClient) {
+		this.webTestClient = webTestClient;
+	}
+
 	@Test
-	void contextLoads() {
+	void whenPostRequestThenBookCreated() {
+		var expectedBook = new Book("1231231231", "Title", "Author", 9.90);
+		webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(expectedBook)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(Book.class)
+				.value(actualBook -> {
+					assertThat(actualBook).isNotNull();
+					assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
+
+				});
 	}
 
 }
